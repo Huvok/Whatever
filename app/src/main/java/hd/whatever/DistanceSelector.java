724d873a60cbf0bac81bba47c698a0df6ef03c2c
Hugo Garcia <hugo_os_95@hotmail.com>
2016-08-27 11:37:29 -0500

    1.0.1 Empieza vista vacia de maps

--- app/src/main/java/hd/whatever/DistanceSelector.java
+++ app/src/main/java/hd/whatever/DistanceSelector.java
@@ -1,19 +1,25 @@
 package hd.whatever;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import hd.whatever.Subcategories.RestSubcategory;
+
 //======================================================================================================================
-public class DistanceSelector extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener
+public class DistanceSelector extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener
 {
 
     //------------------------------------------------------------------------------------------------------------------
     //                                                      //PROPERTIES
     private SeekBar seekBar;
     private TextView textView;
+    private Button btnAccept;
 
     private int intProgress = 0;
 
@@ -29,9 +35,11 @@ public class DistanceSelector extends AppCompatActivity implements SeekBar.OnSee
 
         seekBar = (SeekBar) findViewById(R.id.seekBar);
         textView = (TextView) findViewById(R.id.textView);
+        btnAccept = (Button) findViewById(R.id.btnAccept);
 
         textView.setText("Covered: " + seekBar.getProgress() + "/" + seekBar.getMax());
         seekBar.setOnSeekBarChangeListener(this);
+        btnAccept.setOnClickListener(this);
     }
 
     //------------------------------------------------------------------------------------------------------------------
@@ -62,6 +70,16 @@ public class DistanceSelector extends AppCompatActivity implements SeekBar.OnSee
         )
     {
 
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    @Override
+    public void onClick(
+        View v
+        )
+    {
+        Intent intentSubCategory = new Intent(DistanceSelector.this, TripMap.class);
+        startActivity(intentSubCategory);
     }
 
     //------------------------------------------------------------------------------------------------------------------
